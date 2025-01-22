0296ecb20a19ae82e52afd02e737d0460ac5860a
K-Uzzle <k.uzzle@wustl.edu>
2025-01-22 14:05:43 -0600

    Finished Studio 1

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,19 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("What year is it?");
+		int year = in.nextInt();
+		boolean four = (year % 4 == 0); 
+		boolean hundred = (year % 100 != 0);
+		boolean fourhundred = (year % 400 == 0);
+		boolean leapyear = ((four&&hundred) || fourhundred); 
+		System.out.println(year + " is a leap year: " + leapyear);
+	}
+
+}
